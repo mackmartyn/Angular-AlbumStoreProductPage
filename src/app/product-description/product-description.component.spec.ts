836443b398605597aef084d3bd8eebee0deb7113
mackmartyn <mackmartyn@hotmail.com>
2019-04-17 16:36:17 -0400

    Completed module 3

--- src/app/product-description/product-description.component.spec.ts
+++ src/app/product-description/product-description.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ProductService } from 'app/product.service';
 import { ProductDescriptionComponent } from './product-description.component';
 
 describe('ProductDescriptionComponent', () => {
